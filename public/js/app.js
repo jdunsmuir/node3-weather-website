dc8cfdb032f6125a99825bb6276e5eae7fdf7812
jdunsmuir <50047655+jdunsmuir@users.noreply.github.com>
2020-11-18 14:02:52 +0000

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,7 @@
-console.log('Client side javascript loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
